37956b43cf60ccc8df183e77644b3efe33f08eeb
nidhid1 <93822018+nidhid1@users.noreply.github.com>
2021-12-06 18:12:24 +0530

    Add files via upload

--- z.py
+++ z.py
@@ -17,15 +17,6 @@ from sklearn.compose import make_column_selector as selector
 from sklearn.linear_model import SGDClassifier
 from sklearn import metrics
 i=0
-"""with open('C:/Users/nidhi/Downloads/train(2).csv','r')as file:
-    my_reader=csv.reader(file,delimiter=',')
-    for row in my_reader:
-        if(i>1500):
-            break
-        print(i,row)
-        i+=1
-
-"""
 df=pd.read_csv('C:/Users/nidhi/Downloads/train(2).csv',nrows=3000)
 hour=[]
 for time in df['Dates']:
@@ -71,16 +62,46 @@ for time in df1['Dates']:
 df1['Time']=hour
 print(df1.head(10))
 df1=df1.drop(['Dates','Id'],axis=1)
+days_list=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+address_list=list(np.unique(df['Address']))
+pdDist_list=list(np.unique(df['PdDistrict']))
+def transform(df,l):
+    z=[]
+    for i in df:
+        if(i not in l):
+            l.append(i)
+        g=l.index(i)
+        z.append(g)
+    return z
+    
 y_pred=m.predict(df1)
 print(y_pred)
+classes=['ARSON','ASSAULT','BAD CHECKS','BRIBERY','BURGLARY','DISORDERLY CONDUCT','DRIVING UNDER THE INFLUENCE','DRUG/NARCOTIC','DRUNKENNESS','EMBEZZLEMENT','EXTORTION','FAMILY OFFENSES','FORGERY/COUNTERFEITING',\
+         'FRAUD','GAMBLING','KIDNAPPING','LARCENY/THEFT','LIQUOR LAWS','LOITERING','MISSING PERSON','NON-CRIMINAL','OTHER OFFENSES','PORNOGRAPHY','PORNOGRAPHY/OBSCENE MAT','PROSTITUTION','RECOVERED VEHICLE','ROBBERY',\
+         'RUNAWAY','SECONDARY CODES','SEX OFFENSES FORCIBLE','SEX OFFENSEES NON FORCIBLE','STOLEN PROPERTY','SUICIDE','SUSPICIOUS OCC','TREA','TRESPASS','VANDALISM','VEHICLE THEFT','WARRANTS','WEAPON LAWS']
+c_len=[i for i in range(40)]
+category_list=np.unique(classes)
 #print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-print("V",X)
-p=pd.DataFrame(preprocessor.fit_transform(X))
-print(p.head())
-pipeline1=make_pipeline(preprocessor,SGDClassifier(random_state=42, max_iter=1000, tol=1e-3))
-pipeline1.partial_fit(x_train,y_train)
-pipeline1.partial_fit(x_test,y_test)
-y_pred=pipeline1.predict(df1)
+#data=categorical_preprocessor.fit_transform(df['Category'])
+df['Category']=transform(df['Category'],classes)
+df['DayOfWeek']=transform(df['DayOfWeek'],days_list)
+df['Address']=transform(df['Address'],address_list)
+df['PdDistrict']=transform(df['PdDistrict'],pdDist_list)
+y=df['Category']
+print(df['DayOfWeek'].head(10))
+X=df.drop(columns=['Category'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+for i in y_test:
+    if(i not in range(len(classes))):
+        print(i)
+sgd_clf=SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
+sgd_clf.partial_fit(X_train,y_train,classes=c_len)
+sgd_clf.partial_fit(X_test,y_test)
+#df1['Category']=transform(df1['Category'],classes)
+df1['DayOfWeek']=transform(df1['DayOfWeek'],days_list)
+df1['Address']=transform(df1['Address'],address_list)
+df1['PdDistrict']=transform(df1['PdDistrict'],pdDist_list)
+y_pred=sgd_clf.predict(df1)
 print(y_pred)
 
 
